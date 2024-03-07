4eef6eafdd435233a600c95e0f9afab00ad8c105
Dmcimi <61643603+dmcimi@users.noreply.github.com>
2024-03-07 15:14:12 +0800

    Update get_source_env.sh

--- buildScript/lib/core/get_source_env.sh
+++ buildScript/lib/core/get_source_env.sh
@@ -1,5 +1,5 @@
 if [ ! -z $ENV_NB4A ]; then
-  export COMMIT_SING_BOX_EXTRA="bd07dbeca505668da38741b8fec87489c74275b4"
+  export COMMIT_SING_BOX_EXTRA="e5d208ff4f1993e2341c7c4d31614472fc8111b7"
 fi
 
 if [ ! -z $ENV_SING_BOX_EXTRA ]; then
