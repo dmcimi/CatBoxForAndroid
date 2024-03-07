81399e59a14c26558c9faf376ae04af87f6848f0
Dmcimi <61643603+dmcimi@users.noreply.github.com>
2024-03-07 16:05:48 +0800

    Update env.sh

--- buildScript/init/env.sh
+++ buildScript/init/env.sh
@@ -10,11 +10,11 @@ fi
 
 DEPS=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin
 
-export ANDROID_ARM_CC=$DEPS/armv7a-linux-androideabi21-clang
-export ANDROID_ARM_CXX=$DEPS/armv7a-linux-androideabi21-clang++
-export ANDROID_ARM_CC_21=$DEPS/armv7a-linux-androideabi21-clang
-export ANDROID_ARM_CXX_21=$DEPS/armv7a-linux-androideabi21-clang++
-export ANDROID_ARM_STRIP=$DEPS/arm-linux-androideabi-strip
+# export ANDROID_ARM_CC=$DEPS/armv7a-linux-androideabi21-clang
+# export ANDROID_ARM_CXX=$DEPS/armv7a-linux-androideabi21-clang++
+# export ANDROID_ARM_CC_21=$DEPS/armv7a-linux-androideabi21-clang
+# export ANDROID_ARM_CXX_21=$DEPS/armv7a-linux-androideabi21-clang++
+# export ANDROID_ARM_STRIP=$DEPS/arm-linux-androideabi-strip
 
 export ANDROID_ARM64_CC=$DEPS/aarch64-linux-android21-clang
 export ANDROID_ARM64_CXX=$DEPS/aarch64-linux-android21-clang++
